d3f4523cfe2a22445d2607014ed41281b5f6baf6
zbx1425 <support@zbx1425.cn>
2024-11-03 22:51:35 +0800

    Add SAFEGE rail beams

--- jiachen/assets/mtr/zbx1425/cxd106/models.js
+++ jiachen/assets/mtr/zbx1425/cxd106/models.js
@@ -23,18 +23,8 @@ idTexConnector = Resources.idRelative("s_train_connector.png");
 // Bogie
 
 rawModelBogieBase = ModelManager.loadRawModel(Resources.manager(),
-  Resources.idRelative("bogie_base.obj"), null);
+  Resources.idRelative("bogie.obj"), null);
 rawModelBogieBase.applyUVMirror(false, true);
+rawModelBogieBase.applyScale(1, 1.2, 1);
+rawModelBogieBase.applyTranslation(0, -0.2, 0);
 modelBogie = ModelManager.uploadVertArrays(rawModelBogieBase);
-
-rawModelBogieAir = ModelManager.loadRawModel(Resources.manager(),
-  Resources.idRelative("bogie_rocket.obj"), null);
-rawModelBogieAir.applyUVMirror(false, true);
-rawModelBogieAir.append(rawModelBogieBase);
-modelBogieAir = ModelManager.uploadVertArrays(rawModelBogieAir);
-
-rawModelBogieWheel = ModelManager.loadRawModel(Resources.manager(),
-  Resources.idRelative("bogie_wheel.obj"), null);
-rawModelBogieWheel.applyUVMirror(false, true);
-rawModelBogieWheel.applyTranslation(0, -1.3723, 1);
-modelBogieWheel = ModelManager.uploadVertArrays(rawModelBogieWheel);
