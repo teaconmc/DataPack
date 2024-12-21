f0f9865aefa95bbcf449c3546ce505ecaa267d7c
BX Zhang <support@zbx1425.cn>
2024-12-22 01:11:58 +0800

    Add announcements (#246)

---
+++ jiachen/assets/mtr/zbx1425/cxd106/newpis/ann.js
@@ -0,0 +1,61 @@
+
+function playAnn(ctx, state, train) {
+  if (state.posPhase.stateNowFirst()) {
+    let stations = train.getThisRoutePlatforms();
+    if (stations.size() <= 1) return;
+    let nextIndex = train.getThisRoutePlatformsNextIndex();
+    if (nextIndex <= 0 || nextIndex >= stations.size()) return;
+
+    let stationConfig = getStationConfig(stations, nextIndex);
+    let prevStationConfig = nextIndex > 0 ? getStationConfig(stations, nextIndex - 1) : {};
+
+    // let distLastStation = train.railProgress() - stations.get(nextIndex - 1).distance;
+    // let distNextStation = stations.get(nextIndex).distance - train.railProgress();
+    let isLoopLine = !!stationConfig["isLoopLine"];
+    let soundToPlay = "";
+
+    let isTerminating, isThroughRunning;
+    if (nextIndex == stations.size() - 1 && !isLoopLine) {
+      let allRouteStations = train.getAllPlatforms();
+      let allRouteNextIndex = train.getAllPlatformsNextIndex();
+      if (allRouteNextIndex < allRouteStations.size() - 1
+        && allRouteStations.get(allRouteNextIndex + 1).route.color != allRouteStations.get(allRouteNextIndex).route.color) {
+        isTerminating = false;
+        isThroughRunning = true;
+      } else {
+        isTerminating = true;
+        isThroughRunning = false;
+      }
+    } else {
+      isTerminating = false;
+      isThroughRunning = false;
+    }
+
+    let rteStaCode = stationConfig.routeStaCode;
+    if (!!stationConfig.annRouteName) {
+      rteStaCode = stationConfig.annRouteName + "_" + stationConfig.staCode;
+    }
+
+    if (state.posPhase.stateNow() == "rte") {
+      if (isTerminating) {
+        soundToPlay = rteStaCode + "_next_term";
+      } else if (isThroughRunning) {
+        soundToPlay = rteStaCode + "_next_thru";
+      } else if (!!prevStationConfig["specDep"]) {
+        soundToPlay = rteStaCode + "_next_specdep";
+      } else {
+        soundToPlay = rteStaCode + "_next";
+      }
+    } else if (state.posPhase.stateNow() == "appr") {
+      if (isTerminating) {
+        soundToPlay = rteStaCode + "_arr_term";
+      } else {
+        soundToPlay = rteStaCode + "_arr";
+      }
+    }
+
+    if (soundToPlay != "") {
+      ctx.playAnnSound(Resources.identifier("mtr:ann_" + soundToPlay), 1, 1);
+    }
+  }
+}
